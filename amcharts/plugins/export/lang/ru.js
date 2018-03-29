8d3c848b7b8d888b3547ae73b3132c961a06c177
Roman Murashov <roman-murashov@users.noreply.github.com>
2018-03-29 14:17:08 +0300

    Russian locale for export plugin

---
+++ amcharts/plugins/export/lang/ru.js
@@ -0,0 +1,31 @@
+AmCharts.translations[ "export" ][ "ru" ] = {
+	"fallback.save.text": "CTRL + C - копировать текст в буфер обмена",
+	"fallback.save.image": "Правая кнопка мыши -> Сохранить картинку как... - скачать изображение.",
+
+	"capturing.delayed.menu.label": "{{duration}}",
+	"capturing.delayed.menu.title": "Кликните для отмены",
+
+	"menu.label.print": "Печать",
+	"menu.label.undo": "Отменить",
+	"menu.label.redo": "Повторить",
+	"menu.label.cancel": "Отмена",
+
+	"menu.label.save.image": "Сохранить как изображение...",
+	"menu.label.save.data": "Экспорт данных...",
+
+	"menu.label.draw": "Добавить заметку на графике...",
+	"menu.label.draw.change": "Правка...",
+	"menu.label.draw.add": "Добавить...",
+	"menu.label.draw.shapes": "Фигуру...",
+	"menu.label.draw.colors": "Цвет...",
+	"menu.label.draw.widths": "Толщина кисти...",
+	"menu.label.draw.opacities": "Прозрачность...",
+	"menu.label.draw.text": "Текстовый блок",
+
+	"menu.label.draw.modes": "Инструменты...",
+	"menu.label.draw.modes.pencil": "Карандаш",
+	"menu.label.draw.modes.line": "Линия",
+	"menu.label.draw.modes.arrow": "Линия со стрелкой",
+
+	"label.saved.from": "Источник диаграммы: "
+}
